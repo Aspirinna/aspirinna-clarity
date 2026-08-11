9991193f13ae75bbf02403f6621ce26afc8b2b55
Aspirinna <aspirin121235@outlook.com>
2026-08-12 00:53:08 +0800

    优化即刻

--- app/pages/essays.vue
+++ app/pages/essays.vue
@@ -42,20 +42,28 @@ const list = computed(() =>
 			{{ item.content }}
 		</p>
 
-		<div v-if="item.images?.length" class="images">
+		<div
+			v-if="item.images?.length"
+			class="images"
+			:class="`count-${item.images.length}`"
+		>
 			<ContentPic
 				v-for="(image, index) in item.images"
 				:key="image"
+				class="image"
 				:src="image"
 				:alt="`动态图片 ${index + 1}`"
 			/>
 		</div>
 
-		<footer>
-			<span v-for="tag in item.tags" :key="tag">
-				#{{ tag }}
-			</span>
-			<span v-if="item.location">
+		<footer class="moment-bottom">
+			<div class="moment-tags">
+				<span v-for="tag in item.tags" :key="tag" class="tag">
+					<Icon name="tabler:hash" />{{ tag }}
+				</span>
+			</div>
+
+			<span v-if="item.location" class="location">
 				<Icon name="tabler:map-pin" />{{ item.location }}
 			</span>
 		</footer>
@@ -92,8 +100,31 @@ const list = computed(() =>
 
 	.images {
 		display: grid;
-		grid-template-columns: repeat(auto-fit, minmax(140px, 1fr));
-		gap: 0.5rem;
+		grid-template-columns: repeat(3, minmax(0, 1fr));
+		gap: 4px;
+
+		&.count-1 {
+			grid-template-columns: minmax(0, 62%);
+		}
+
+		&.count-2, &.count-4 {
+			grid-template-columns: repeat(2, minmax(0, 1fr));
+		}
+
+		.image {
+			margin: 0;
+
+			:deep(figcaption) {
+				display: none;
+			}
+
+			:deep(img) {
+				width: 100%;
+				max-height: 70vh;
+				border-radius: 0.4rem;
+				object-fit: contain;
+			}
+		}
 	}
 
 	footer {
@@ -103,5 +134,36 @@ const list = computed(() =>
 		font-size: 0.85em;
 		color: var(--c-text-2);
 	}
+}
+
+.moment-bottom {
+	display: flex;
+	flex-wrap: wrap;
+	align-items: center;
+	justify-content: space-between;
+	gap: 0.5em;
+	font-size: 0.8em;
+	color: var(--c-text-3);
+}
+
+.moment-tags {
+	display: flex;
+	flex-wrap: wrap;
+	gap: 0.5em;
+
+	.tag {
+		display: inline-flex;
+		align-items: center;
+		padding: 0.1em 0.6em;
+		border-radius: 1em;
+		background-color: var(--c-bg-2);
+		color: var(--c-primary);
+	}
+}
+
+.location {
+	display: inline-flex;
+	align-items: center;
+	gap: 0.1em;
 }
 </style>
