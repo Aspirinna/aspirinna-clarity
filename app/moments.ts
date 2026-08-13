4918993ac39a52aa277de33f177d0e0b48205d8d
Aspirinna <aspirin121235@outlook.com>
2026-08-13 18:46:54 +0800

    test2

--- app/moments.ts
+++ app/moments.ts
@@ -8,8 +8,13 @@ export interface MomentItem {
 
 export const moments: MomentItem[] = [
 	{
+		content: '开始学习 Nuxt 和 Vue。',
+		createdAt: '2026-08-12T1:30:00+08:00',
+		tags: ['Nuxt', '学习'],
+	},
+	{
 		content: '博客的评论系统终于部署完成了...发一下好久之前的摄影测试一下。',
-		createdAt: '2025-08-11T20:00:00+08:00',
+		createdAt: '2026-08-11T20:00:00+08:00',
 		images: [
 			'https://img.aspirinna.cloud/posts/20260811185228297.png',
 		],
